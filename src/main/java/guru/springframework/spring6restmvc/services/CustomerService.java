36f2bfec0b61538fc5dadb156d6463fa1a28e3ee
andvardy <avmusss@gmail.com>
2023-09-04 10:46:39 +0200

    HTTP REST

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public interface CustomerService {
+
+    Customer getCustomerById(UUID uuid);
+
+    List<Customer> getAllCustomers();
+
+    Customer saveNewCustomer(Customer customer);
+
+    void updateCustomerById(UUID customerId, Customer customer);
+
+    void deleteCustomerById(UUID customerId);
+
+    void patchCustomerById(UUID customerId, Customer customer);
+}
